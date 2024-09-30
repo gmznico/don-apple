eb89a4d7463947b465551985fb159702a2b34d5f
gmznico <niko34241@gmail.com>
2024-09-30 19:44:15 -0300

    arreglando bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Header from './components/Header';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTruck, faPercent, faHeadphones } from '@fortawesome/free-solid-svg-icons';
@@ -12,37 +13,7 @@ import './index.css';
 
 const HomePage = () => (
   <>
-    <header id='inicio'>
-      <nav className="navbar container">
-        <div>
-          <img className="logo" src="img/logos/logo-black.png" alt="Don Apple" />
-        </div>
-        <div className="navbar-menu">
-          <a href="#inicio">INICIO</a>
-          <a href="#products">PRODUCTOS</a>
-          <a href="#about">NOSOTROS</a>
-          <a href="#cuestions">PREGUNTAS</a>
-        </div>
-      </nav>
-
-      <ScrollReveal>
-        <section className="hero">
-          <div className="hero-content container">
-            <img className="hero-img" src="img/products/iphonegrande.png" alt="iPhone" />
-            <div className="hero-text">
-              <h2>¡Todo lo que buscas!</h2>
-              <h3>Venta por mayor y menor</h3>
-              <div className="hero-p">
-                <p>Equipos Apple | Nuevos y Usados</p>
-                <button className="hero-button button">
-                  <span>Ver más</span>
-                </button>
-              </div>
-            </div>
-          </div>
-        </section>
-      </ScrollReveal>
-    </header>
+    <Header/>
 
     <main className="container">
       <ScrollReveal>
